a8c49c5bd21dbb120bdd2ca9a7001f258e78a245
Joe Hilgard <jhilgard@asc.upenn.edu>
2015-12-03 15:40:18 -0500

    Added Egger test and inspections of heterogeneity.

--- Analysis.R
+++ Analysis.R
@@ -18,6 +18,8 @@ PET(data)
 PEESE(data)
 funnelPETPEESE(data)
 
+# IL-6 as biomarker of severity?
+
 # Plot was exported manually on 2015-12-03 with 600x600 resolution.
 
 # Sensitivity analysis excluding outlier study by Gou et al.
@@ -26,4 +28,33 @@ forest(res2)
 funnel(res2)
 trimfill(res2)
 funnel(trimfill(res2))
+PET(data[data$study != "Guo 2012", ])
+PEESE(data[data$study != "Guo 2012", ])
 funnelPETPEESE(data[data$study != "Guo 2012", ])
+
+egger2 <- rma(yi = smd, sei = sei, data = data[data$study != "Guo 2012", ], slab=study,
+              mods = ~sei)
+summary(egger2) # significant, p = .019
+
+# Other considerations ----
+# Correlation btwn moderators, MDD diagnosis and medication
+table(data$mdd, data$medication, dnn = list("MDD", "Medication"))
+
+# Heterogeneity
+funnel(res, pch = ifelse(data$medication == "psychotropic", 15, 17))
+funnel(res, pch = ifelse(data$mdd == "without", 15, 17))
+
+# PET and PEESE within moderator groups
+PET(data[data$medication == "free",])
+PEESE(data[data$medication == "free",])
+funnelPETPEESE(data[data$medication == "free",])
+PET(data[data$medication == "psychotropic",])
+PEESE(data[data$medication == "psychotropic",])
+funnelPETPEESE(data[data$medication == "psychotropic",])
+
+PET(data[data$mdd == "without",])
+PEESE(data[data$mdd == "without",])
+funnelPETPEESE(data[data$mdd == "without",])
+PET(data[data$mdd == "with",])
+PEESE(data[data$mdd == "with",])
+funnelPETPEESE(data[data$mdd == "with",])
